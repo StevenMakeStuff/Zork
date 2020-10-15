c7d88f1c4e8f92741dd9b0fae23c1bb5f6623d00
StevenMakeStuff <54723295+StevenMakeStuff@users.noreply.github.com>
2020-10-15 03:44:52 -0400

    Commit for school access.
    Made tons of changes, but still need to work on at school.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,134 +7,20 @@ namespace Zork
 {
     class Program
     {
-
-        private enum Fields
+        static void Main(string[] args)
         {
-            Name = 0,
-            Description
+            const string defaultGameFileName = "Zork.json";
+            string gameFileName = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFileName);
+
+            Game game = Game.Load(gameFileName);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
 
         private enum CommandLineArguments
         {
-            RoomsFilename = 0
+            GameFilename = 0
         }
-
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
-
-        
-
-        static void Main(string[] args)
-        {
-            const string defaultRoomsFilename = "Rooms.Json";
-            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRoom(roomsFilename);
-
-            Console.WriteLine($"Welcome to Zork!");
-
-
-            Room previousRoom = null;
-            Commands command = Commands.UNKNOWN;
-            while(command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-                if (previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                switch (command)
-                {
-                    case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-                                //Moves the character should an input return true.
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if(Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut!");
-                        }
-                        break;
-
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-                }
-            }
-        }
-                //Makes sure that the player is entering a valid string
-
-
-        private static bool Move(Commands command)
-        {       //checks to see if moving is possible
-            Assert.IsTrue(IsDirection(command), "Invalid direction.");
-
-            bool isValidMove = true;
-            switch (command)
-            {
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    isValidMove = false;
-                    break;
-            }
-
-            return isValidMove;
-        }
-
-        private static Commands ToCommand(string commandString) =>
-            Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static Room[,] Rooms = {
-            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") },
-        };
-
-        private static void InitializeRoom(string roomsFilename)
-        {
-            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-        }  
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST,
-        };
-
-        private static (int Row, int Column) Location = (1, 1);
     }
 }
